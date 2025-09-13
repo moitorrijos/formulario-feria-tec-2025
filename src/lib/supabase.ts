220ff835d490790b63930a4596ce90dd2dcb6cd9
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2025-09-13 14:46:02 -0500

    Update Supabase client initialization to use SERVICE_ROLE_KEY instead of ANON_KEY

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   import.meta.env.SUPABASE_URL,
-  import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  import.meta.env.SUPABASE_SERVICE_ROLE_KEY,
 );
